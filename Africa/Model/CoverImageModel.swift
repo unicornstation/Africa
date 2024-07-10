516131ef531772936dc4f12ab4556919e6ae0a2e
Alyona <alyona.createbox@gmail.com>
2024-07-10 17:17:20 +0300

    Decode a json file

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Alyona Voronina on 10.07.2024.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
