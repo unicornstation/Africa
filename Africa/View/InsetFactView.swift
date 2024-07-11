90689d8fb6e950ee147f7d003a31446bd41715ac
Alyona <alyona.createbox@gmail.com>
2024-07-11 18:29:32 +0300

    InsetFactView

---
+++ Africa/View/InsetFactView.swift
@@ -0,0 +1,18 @@
+//
+//  InsetFactView.swift
+//  Africa
+//
+//  Created by Alyona Voronina on 11.07.2024.
+//
+
+import SwiftUI
+
+struct InsetFactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InsetFactView()
+}
