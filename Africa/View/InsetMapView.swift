bea694f53550edc5e4b006cd6c29fd28c4b8c95e
Alyona <alyona.createbox@gmail.com>
2024-07-15 19:13:45 +0300

    InsetMapView

---
+++ Africa/View/InsetMapView.swift
@@ -0,0 +1,18 @@
+//
+//  InsetMapView.swift
+//  Africa
+//
+//  Created by Alyona Voronina on 15.07.2024.
+//
+
+import SwiftUI
+
+struct InsetMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InsetMapView()
+}
