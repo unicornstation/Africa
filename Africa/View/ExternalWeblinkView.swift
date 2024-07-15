bad65cd4c8b438bcde68f954aad4e67c9df992c2
Alyona <alyona.createbox@gmail.com>
2024-07-15 19:54:53 +0300

    Added weblink

---
+++ Africa/View/ExternalWeblinkView.swift
@@ -0,0 +1,18 @@
+//
+//  ExternalWeblinkView.swift
+//  Africa
+//
+//  Created by Alyona Voronina on 15.07.2024.
+//
+
+import SwiftUI
+
+struct ExternalWeblinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExternalWeblinkView()
+}
