8d8ee16255490b972a5e9ff524defbada120becf
Alyona <alyona.createbox@gmail.com>
2024-07-15 19:20:26 +0300

    InsetMapView

--- Africa/View/InsetMapView.swift
+++ Africa/View/InsetMapView.swift
@@ -6,13 +6,51 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct InsetMapView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599), span: MKCoordinateSpan(latitudeDelta: 60.0, longitudeDelta: 60.0))
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+     // Map(coordinateRegion: $region)
+        Map(initialPosition: .region(region))
+            .overlay(NavigationLink(destination: MapView()) {
+                    HStack {
+                        Image(systemName: "mappin.circle")
+                            .foregroundColor(Color.white)
+                            .imageScale(.large)
+                        
+                        Text("Locations")
+                            .foregroundColor(.accentColor)
+                            .fontWeight(.bold)
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal,14)
+                    .background(Color.black
+                    .opacity(0.4)
+                    .cornerRadius(8)
+                                )
+                } //: NAVIGATION
+                .padding(12)
+                     , alignment: .topLeading
+                )
+            
+        
+            .frame(height: 256)
+            .cornerRadius(12)
+    }
+}
+// MARK: - PREVIEW
+struct InsetMapView_Preview: PreviewProvider {
+    
+   static var previews: some View {
+        InsetMapView()
+           .previewLayout(.sizeThatFits)
+           .padding()
     }
 }
 
-#Preview {
-    InsetMapView()
-}
