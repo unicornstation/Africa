8561689404251139dd0cbaf0908e7cedeb1224a9
Alyona <alyona.createbox@gmail.com>
2024-07-17 19:04:13 +0300

    added LocationModel

---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Alyona Voronina on 17.07.2024.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
