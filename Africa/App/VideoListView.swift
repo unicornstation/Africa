3c77128647d9197b2092b069ec3ac58907719a25
Alyona <alyona.createbox@gmail.com>
2024-07-17 18:10:29 +0300

    added shuffle for videos (VideoListView)

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct VideoListView: View {
     // MARK: - PROPERTIES
     
-    var videos: [Video] = Bundle.main.decode("videos.json")
+  @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
     
     // MARK: - BODY
     var body: some View {
@@ -18,9 +20,22 @@ struct VideoListView: View {
             List {
                 ForEach(videos) { item in
                     VideoListItemView(video: item)
-                }
-                
+                        .padding(.vertical, 8)
+                } //: LOOP
             } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        //Shaffle videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
         } //: NAVIGATION
     }
 }
