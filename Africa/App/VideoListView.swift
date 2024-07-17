f996a2d4eab2eaef3a364da5204f6ad26f7250e1
Alyona <alyona.createbox@gmail.com>
2024-07-17 17:42:19 +0300

    VideoListView

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - PROPERTIES
+    
+    var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                }
+            } //: LIST
+        } //: NAVIGATION
     }
 }
 
-#Preview {
-    VideoListView()
+// MARK: - PREVIEW
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+            .previewDevice("iPhone 15 Pro")
+    }
 }
--- Africa/View/VideoListItemView.swift
+++ Africa/View/VideoListItemView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct VideoListItem: View {
+struct VideoListItemView: View {
     // MARK: - PROPERTIES
     
     let video: Video
@@ -49,7 +49,7 @@ struct VideoListItem_Preview: PreviewProvider {
     static let videos: [Video] = Bundle.main.decode("videos.json")
    
     static var previews: some View {
-        VideoListItem(video: videos[0])
+        VideoListItemView(video: videos[0])
             .previewLayout(.sizeThatFits)
             .padding()
     }
