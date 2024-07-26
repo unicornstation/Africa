c318f48e0673f49e442b65aaf244d0d52e02a185
Alyona <alyona.createbox@gmail.com>
2024-07-26 17:04:18 +0300

    added Buttons for Grid

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,34 +8,102 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        // MARK: - PROPERTIES
-        
-        let animals: [Animal] = Bundle.main.decode("animals.json")
-        
-        // MARK: - BODY
-        NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal))  {
-                        AnimalListItemView(animal: animal)
-                    } //Link
-                } //Loop
-                }//: List
-                .navigationBarTitle("Africa", displayMode: .large)
-            }//: Navigation
-        }
-    }
-
+    // MARK: - PROPERTIES
     
-    struct ContentView_Preview: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-                .previewDevice("iPhone 15 Pro")
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var isGridViewActive: Bool = false
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Int = 1
+    @State private var toolBarIcon: String = "square.grid.2x2"
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        print("Grid Number: \(gridColumn)")
+        
+        // Tolbar image
+        switch gridColumn {
+        case 1: toolBarIcon = "square.grid.2x2"
+        case 2: toolBarIcon = "square.grid.3x2"
+        case 3: toolBarIcon = "rectangle.grid.1x2"
+        default:
+            toolBarIcon = "square.grid.2x2"
         }
     }
+    
+    var body: some View {
+        // MARK: - BODY
+        
+        NavigationView {
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal))  {
+                                AnimalListItemView(animal: animal)
+                            } //Link
+                        } //Loop
+                    }//: List
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                    AnimalGridItemView(animal: animal)
+                                }//: Link
+                            }//: Loop
+                        }//: Grid
+                        .padding(10)
+                        
+                        /*  'animation' was deprecated in iOS 15.0: Use withAnimation or animation(_:value:) instead. */
+                        .animation(.easeIn, value: gridColumn)
+                    }//: Scroll
+                }//: Condition
+            }//: GROUP
+            .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        //LIST
+                        Button(action: {
+                            print("List view is activated")
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        //GRID
+                        Button(action: {
+                            print("Grid view is activated")
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                            gridSwitch()
+                        }) {
+                            Image(systemName: toolBarIcon)
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                    }//: HStack
+                }
+            }
+        }//: Navigation
+    }
+}
+
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 15 Pro")
+    }
+}
 
